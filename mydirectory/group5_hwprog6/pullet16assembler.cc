71573fa0da0545f13fee7f8e88dfd271a8340cb2
Tsung Wei Wu <tsungweiwu@gmail.com>
2018-12-05 17:17:08 -0500

    comments made

--- mydirectory/group5_hwprog6/pullet16assembler.cc
+++ mydirectory/group5_hwprog6/pullet16assembler.cc
@@ -15,7 +15,6 @@
 **/
 Assembler::Assembler() {
 }
-// trying GitHub
 /***************************************************************************
  * Destructor
 **/
@@ -80,9 +79,9 @@ void Assembler::Assemble(Scanner& in_scanner, string binary_filename,
     int bin_to_dec = c1 * 256;
     int16_t bin_16 = bin_to_dec + c2;
     string bin_to_bitstring = DABnamespace::DecToBitString(bin_16, 16);
-    // dumps to text file
+    // dumps to adotout text file
     out_stream << bin_to_bitstring << endl;
-  }
+  } // end of for loop
 input.close();
 }
 
@@ -151,7 +150,6 @@ void Assembler::PassOne(Scanner& in_scanner) {
   Utils::log_stream << "enter PassOne" << endl;
 #endif
   Utils::log_stream << "PASS ONE" << endl;
-  // Unfinished code -Sean
   found_end_statement_ = 0;
   string lbl = "";
   pc_in_assembler_ = 0;
@@ -166,7 +164,8 @@ void Assembler::PassOne(Scanner& in_scanner) {
 
     if (line.length() < 21) {
       line.append(21-line.length(), ' ');  // pad to length 21, for easier code
-    }
+    }  // end of if
+
     if (line.substr(0, 1) == "*") {
       // set beginning lines of comments
       codeline.SetCommentsOnly(linecounter_, line);
@@ -176,23 +175,22 @@ void Assembler::PassOne(Scanner& in_scanner) {
     } else {
       if (line.substr(0, 3) != "   ") {  // reading label
         label = line.substr(0, 3);
-        if (symboltable_.count(label) == 0) {
+        if (symboltable_.count(label) == 0) {  // checks for duplicate
           // add symbol to table if not already present
           symboltable_.insert({label, Symbol(label, pc_in_assembler_)});
-        } else {
-          // sets a flag for duplicate labels
-          symboltable_.at(label).SetMultiply();
-        }
+        } else {  // else found duplicate symbol
+          symboltable_.at(label).SetMultiply();  // sets multiply flag
+        }  // end else
       }  // end of if label
       mnemonic = line.substr(4, 3);  // getting mnemonic
       addr = line.substr(8, 1);  // getting addr
-      if (line.substr(10, 3) != "   ") {
+      if (line.substr(10, 3) != "   ") { // getting symbol
         // if field is empty, value of symoperand_ will be null
         symoperand = line.substr(10, 3);
       }  // end of if symbol
-      hexoperand = line.substr(14, 5);
-      if (line.substr(20, 1) == "*") {
-        comments = line.substr(20);
+      hexoperand = line.substr(14, 5);  // getting hex value
+      if (line.substr(20, 1) == "*") {  // checks if it is a comment
+        comments = line.substr(20);  // getting comment
       }  // end of if comments
       // sends all the data for it to be assigned
       codeline.SetCodeLine(linecounter_, pc_in_assembler_, label,
@@ -200,23 +198,23 @@ void Assembler::PassOne(Scanner& in_scanner) {
       int jumpvalue = codeline.GetHexObject().GetValue();
       if (mnemonic == "ORG") {
         if (!(jumpvalue > 4095 || jumpvalue < 0)) {
-          pc_in_assembler_ = jumpvalue;
-        }
+          pc_in_assembler_ = jumpvalue;  // ORG to hex value
+        }  // end of if ORG
       } else if (mnemonic == "DS ") {
         if (!((jumpvalue + pc_in_assembler_) > 4095 || jumpvalue < 1)) {
-        pc_in_assembler_ += jumpvalue;
-        }
+        pc_in_assembler_ += jumpvalue;  // DS to hex value
+        }  // end of if DS
       } else if (mnemonic != "END") {
-        pc_in_assembler_++;
+        pc_in_assembler_++;  // PC increases after every instr
       }  // end of if END
 
       if (mnemonic == "END") {
-        found_end_statement_ = true;
+        found_end_statement_ = true;  // found end which sets has_error false
       }  // end of if statement END
     }  // end of else statement
     linecounter_++;
     codelines_.push_back(codeline);
-    line = in_scanner.NextLine();
+    line = in_scanner.NextLine();  // will get next line until file is empty
   }  // end of while loop
 
 #ifdef EBUG
@@ -237,9 +235,9 @@ void Assembler::PassTwo() {
   string err;
   Symbol symbol;
   Hex hex;
-// This block goes through each of the inputted codelines and converts the
-// mnemonic, direct/indirect addressing, and 12 bit address to appropriate
-// machine code.
+  // This block goes through each of the inputted codelines and converts the
+  // mnemonic, direct/indirect addressing, and 12 bit address to appropriate
+  // machine code.
   for (auto it = codelines_.begin(); it != codelines_.end(); it++) {
     // If the line is a comment, then ignore
     if ((*it).IsAllComment()) {
@@ -257,7 +255,7 @@ void Assembler::PassTwo() {
         bitstring += "1";
       } else {
         bitstring += "0";
-      }
+      }  // end of if indirect addr
 
       // The last twelve digits are defined by the symbol or the hex operand,
       // unless it's RD or STP or WRT
@@ -276,12 +274,12 @@ void Assembler::PassTwo() {
           err += GetInvalidMessage("HEX OPERAND " + hex.GetText());
         } else {
           bitstring += DABnamespace::DecToBitString(hex.GetValue(), 12);
-        }
+        }  // end of else
       } else {  // If there was no hex or symbol
         err += "\n***** ERROR -- NO HEX OR SYMBOL OPERAND";
         bitstring = kDummyCodeC;
-      }
-      WriteMemory((*it).GetPC(), bitstring);
+      } // end of else
+      WriteMemory((*it).GetPC(), bitstring);  // for PrintMachineCode
       (*it).SetMachineCode(bitstring);  // for PrintCodeLine
     } else if ((*it).GetMnemonic() == "RD ") {
       // check the other possible opcodes.
@@ -309,18 +307,18 @@ void Assembler::PassTwo() {
         WriteMemory((*it).GetPC() +
             (*it).GetHexObject().GetValue() - 1, kDummyCodeA);
         WriteMemory((*it).GetPC(), kDummyCodeC);
-      }
+      }  // end of if DS
     } else if ((*it).GetMnemonic() == "ORG") {
       if (hex.GetValue() > 4095 || hex.GetValue() < 0) {
         err += GetInvalidMessage("ORG ALLOCATION " + hex.GetText());
-      }
+      }  // end of if ORG
     } else if ((*it).GetMnemonic() == "END") {
       bitstring = kDummyCodeD;
       (*it).SetMachineCode(bitstring);  // for PrintCodeLine
     } else {
       // If everything failed to match, then the mnemonic is not valid.
       err += GetInvalidMessage("MNEMONIC '" + (*it).GetMnemonic() + "\'");
-    }
+    }  // end of else
 
     // Check hex value's error status
     if (hex.HasAnError()) {
@@ -330,29 +328,29 @@ void Assembler::PassTwo() {
     // Check Label for its error flags
     if ((*it).HasLabel()) {
       Symbol label = symboltable_.find((*it).GetLabel()) -> second;
-      if (label.IsInvalid()) {
+      if (label.IsInvalid()) {  // checks for invalid labels
         err += GetInvalidMessage("LABEL '" + (*it).GetLabel() + "\'");
       }
-      if (label.IsMultiply()) {
+      if (label.IsMultiply()) {  // checks for duplication
         err += GetMultiplyMessage("LABEL '" + (*it).GetLabel() + "\'");
       }
-    }
+    }  // end of if HasLabel
 
     // Finally, check the symoperand for its error flags
     if ((*it).HasSymOperand()) {
       auto sym_it = symboltable_.find((*it).GetSymOperand());
-      if (sym_it == symboltable_.end()) {
+      if (sym_it == symboltable_.end()) {  // loops through table
         err += GetUndefinedMessage("\'" + (*it).GetSymOperand() + "\'");
       } else {
         symbol = sym_it -> second;
-        if (symbol.IsInvalid()) {
+        if (symbol.IsInvalid()) {  // checks for invalid symbols
           err += GetInvalidMessage("SYMBOL '" + (*it).GetSymOperand() + "\'");
         }
-        if (symbol.IsMultiply()) {
+        if (symbol.IsMultiply()) {  // checks for duplication
           err += GetMultiplyMessage("SYMBOL '" + (*it).GetSymOperand() + "\'");
         }
       }
-    }
+    }  // end of if HasSymOperand
 
     (*it).SetErrorMessages(err);
   }
@@ -396,9 +394,7 @@ void Assembler::PrintMachineCode(string binary_filename,
   Utils::log_stream << "enter PrintMachineCode" << " "
                     << binary_filename << endl;
 #endif
-// WILL CHANGE THIS TO BE READING THE BINARY MACHINE CODE LATER BUT FOR NOW
-// JUST DUMPING THE ASCII THAT WE HAVE
-// to do find a way to do that kDummyCodeA thing in the middle !!!
+// Dumps Machine Code stored in OneMemoryCode in a pretty format
 Utils::log_stream << "MACHINE CODE\n"
                   << "enter PrintMachineCode " << binary_filename
                   << endl;
